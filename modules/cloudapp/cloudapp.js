93ee67e4fba1444e01d268089a7adc43d15ef919
Matt Burke <spraints@gmail.com>
2012-07-26 22:47:27 -0400

    Customize.

--- modules/cloudapp/cloudapp.js
+++ modules/cloudapp/cloudapp.js
@@ -18,7 +18,7 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
 */
-var displayCloudAppImages = true;
+var displayCloudAppImages = false;
 
 /* 
   Display CloudApp images inline.
